4588319ce39dccb8f6f3cfe3be2a2a81dbe6f8e1
Stimul8d <davidhuntley.personal@googlemail.com>
2016-03-01 00:52:48 +0000

    added load last result from file

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-var voice = require("say");
-var request = require("request");
-var config = require("./config");
+var voice = require('say');
+var request = require('request');
+var fs = require('fs')
+var config = require('./config');
 
 var sendRequest = function() {
 	request(config.pathToJenkins, function(error, res, body) {
-			if (!error && res.statusCode == 200) {
-				var result = JSON.parse(body);
+		if (!error && res.statusCode == 200) {
+			var thisResult = JSON.parse(body);
 
-				var text = result.fullDisplayName + " is " + ((result.building) ? "" : "not ") + "building";
+			fs.readFile(config.latestBuildFile, function(err, data) {
+				
+				var lastResult = JSON.parse(data);
+				
+				fs.writeFile(config.latestBuildFile, body, function(err) {
+					if (err) {
+						console.log(err);
+						return;
+					}
+				});
+
+				if (err) {
+					console.log(err);
+					return;
+				}
+
+				if (thisResult.number <= lastResult.number) {
+					console.log('No new builds');
+					return;
+				}
+
+				var text = thisResult.fullDisplayName + ' is ' + ((thisResult.building) ? '' : 'not ') + 'building';
 
 				console.log(text);
 
 				voice.speak(text);
-			}
-		})
-	};
+			});
+		}
+	})
+};
 
 setInterval(function() {
 	console.log('Sending Request');
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	pathToJenkins: "http://192.168.99.100:32769/job/build1/lastBuild/api/json"
+	pathToJenkins: 'http://192.168.99.100:32769/job/build1/lastBuild/api/json',
+	latestBuildFile: './latestBuild.js'
 }
