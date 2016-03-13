629156d57f909d309b753ae6bd034e2896298dd7
Stimul8d <davidhuntley.personal@googlemail.com>
2016-03-13 22:02:33 +0000

    debug

--- unicorn.js
+++ unicorn.js
@@ -3,29 +3,29 @@ var shell = require('python-shell');
 var chalk = require('chalk');
 require('shelljs/global');
 
-var run = function(fileName, chalk) {
+var run = function (fileName, chalk) {
     if (config.voldermort) {
         console.log(chalk('not running ' + fileName));
         return;
     }
-    exec('python ./stop.py &');
-    exec(fileName + ' &');
-//    shell.run(fileName, function(err) {
-//        if (err) {
-//            console.log(chalk.red(err));
-//        }
-//    })
+    shell.run('python ./stop.py &', function (err) {
+        shell.run(fileName, function (err) {
+            if (err) {
+                console.log(chalk.red(err));
+            }
+        })
+    });
 }
 
 module.exports = {
-    error: function() {
-        exec('python ./scenes/random_blinky.py &');
-        //run('./scenes/rainbow_blinky.py', chalk.red);
+    error: function () {
+        //exec('python ./scenes/random_blinky.py &');
+        run('./scenes/rainbow_blinky.py', chalk.red);
     },
-    inProgress: function() {
+    inProgress: function () {
         run('./scenes/cross.py', chalk.yellow);
     },
-    pass: function() {
+    pass: function () {
         run('./scenes/matrix.py', chalk.green);
     }
 }
