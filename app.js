3ad2dbea104bb60abe0d3c302bdf42c8293a90b7
Stimul8d <davidhuntley.personal@googlemail.com>
2016-03-13 21:31:56 +0000

    debug

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ var sendRequest = function () {
 
     request(jenkins, function (error, res, body) {
         
+        exec('pkill -f ./scenes/cross.py');
+        exec('pkill -f ./scenes/rainbow_blinky.py');
+        exec('pkill -f ./scenes/matrix.py');
+        
         //if there's an error, get out.
         if (error || res.statusCode != 200) {
             announce.BuildFailure('request error');
