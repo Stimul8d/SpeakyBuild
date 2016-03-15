e59fe908bce53ec31bf48487b499950d5f6a715f
Stimul8d <davidhuntley.personal@googlemail.com>
2016-03-15 21:57:56 +0000

    debug

--- scenes/simple.py
+++ scenes/simple.py
@@ -7,9 +7,8 @@ def go():
     UH.clear()
 
     for y in range(8):
-    for x in range(8):
-        UH.set_pixel(x,y,255,0,255)
-        UH.show()
-        time.sleep(0.05)
-
+        for x in range(8):
+            UH.set_pixel(x,y,255,0,255)
+            UH.show()
+            time.sleep(0.05)
     time.sleep(1)
