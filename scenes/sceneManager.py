e1c5e8053d8104b1e2614df9f8a42dfb25296052
Stimul8d <davidhuntley.personal@googlemail.com>
2016-03-15 22:21:27 +0000

    debug

--- scenes/sceneManager.py
+++ scenes/sceneManager.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 #import rainbow_blinky as pass
 import simple as start
+import unicornhat as hat
 import time
 
 start.go()
 print('starting up the hat')
+hat.clear()
 
 t_end = time.time() + 5
 while time.time() < t_end:
