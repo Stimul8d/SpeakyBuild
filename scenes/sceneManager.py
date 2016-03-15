ade22fc064f55803164aaf78c3f3a783493a6540
Stimul8d <davidhuntley.personal@googlemail.com>
2016-03-15 21:51:30 +0000

    starting to write scene manager in python

---
+++ scenes/sceneManager.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import raindbow_blinky as pass_scene
+import simple as start
+import time
+
+start.go()
+
+while True:   
+    time.sleep(1000)
--- scenes/simple.py
+++ scenes/simple.py
@@ -3,12 +3,13 @@
 import unicornhat as UH
 import time
 
-UH.clear()
+def go():
+    UH.clear()
 
-for y in range(8):
-  for x in range(8):
-    UH.set_pixel(x,y,255,0,255)
-    UH.show()
-    time.sleep(0.05)
+    for y in range(8):
+    for x in range(8):
+        UH.set_pixel(x,y,255,0,255)
+        UH.show()
+        time.sleep(0.05)
 
-time.sleep(1)
+    time.sleep(1)
