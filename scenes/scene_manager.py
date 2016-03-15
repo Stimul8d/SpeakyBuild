3b551f4cd67947751f9a0d4a7966229da15089d9
Stimul8d <davidhuntley.personal@googlemail.com>
2016-03-15 23:38:54 +0000

    debug

--- scenes/scene_manager.py
+++ scenes/scene_manager.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import start, stop, inprog, bad, good 
+import start, stop, inprog, bad, good
 import time, threading
 import sys, os.path
 
@@ -9,10 +9,10 @@ lastBuild = 'nothing'
 def loop():
     start.go()
     stop.go()
-    
-    if os.path.isfile(buildFile)    
+
+    if os.path.isfile(buildFile)
         lastBuild = open(buildFile).read()
-        
+
         if lastBuild == 'inprog'
             inprog.go()
         if lastBuild == 'good'
@@ -22,7 +22,7 @@ def loop():
         if lastBuild == 'nothing'
             start.go()
             stop.go()
-        
+
     threading.Timer(1, loop).start()
-    
+
 loop()
