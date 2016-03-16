660be73757a3183782982dabff8afc982da3f582
Stimul8d <davidhuntley.personal@googlemail.com>
2016-03-16 22:21:18 +0000

    hooked up test doubles

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 var chalk = require('chalk');
 var speak = require('./announce');
 var unicorn = require('./unicorn');
+var PythonShell = require('./python-shell');
 require('shelljs/global');
 
-exec('echo "hey there"');
+//exec('echo "hey there"');
 //speak.GoodBuild('hi there');
 //console.log(chalk.red('hi there'));
 //unicorn.pass('hi there');
+var shell = new PythonShell('./scenes/test.py', { mode: 'text ' })
+
+shell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement) 
+    console.log(message);
+});
--- unicorn.js
+++ unicorn.js
@@ -10,7 +10,7 @@ var run = function (fileName, chalk) {
         return;
     }
 
-    var shell = new PythonShell(fileName)
+    var shell = new PythonShell(fileName, { mode: 'text ' })
 
     shell.on('message', function (message) {
         // received a message sent from the Python script (a simple "print" statement) 
@@ -20,8 +20,7 @@ var run = function (fileName, chalk) {
 
 module.exports = {
     initialize: function () {
-        run('./scenes/test.py');
-        //run('./scenes/scene_manager.py');
+        run('./scenes/scene_manager.py');
     },
 }
 
